7657bc604f9e0920d24a289a0157dacdee6f35b3
jessicavc <jessica.vaicav@gmail.com>
2020-09-24 19:47:42 -0700

    fizzbuzz

--- practice.js
+++ practice.js
@@ -5,21 +5,21 @@
 // if divisible by 3 and 5, print "fizz buzz"
 //other print the number itself
 
-// function fizzBuzz (fizzBuzzArray){
-//     for (var i = 0; i<fizzBuzzArray.length; i++) {
-//         if (fizzBuzzArray[i] % 3 == 0 && fizzBuzzArray[i]  % 5 == 0) {
-//             console.log("fizzbuzz");
-//         } else if (fizzBuzzArray[i]  % 3 == 0){
-//             console.log("fizz");
-//         } else if (fizzBuzzArray[i]  % 5 == 0){
-//             console.log("buzz");
-//         } else {
-//             console.log(fizzBuzzArray[i]);
-//         }
-//     }
-// } 
+function fizzBuzz (fizzBuzzArray){
+    for (var i = 0; i<fizzBuzzArray.length; i++) {
+        if (fizzBuzzArray[i] % 3 == 0 && fizzBuzzArray[i]  % 5 == 0) {
+            console.log("fizzbuzz");
+        } else if (fizzBuzzArray[i]  % 3 == 0){
+            console.log("fizz");
+        } else if (fizzBuzzArray[i]  % 5 == 0){
+            console.log("buzz");
+        } else {
+            console.log(fizzBuzzArray[i]);
+        }
+    }
+} 
 
-// fizzBuzz([6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+fizzBuzz([6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 
 
 // given an array with unknown amount of numbers
